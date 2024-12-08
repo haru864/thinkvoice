6a39f47d9794d84efb998a6e0801cd7a7960686e
haru <kawachigenji95@gmail.com>
2024-12-08 23:58:43 +0900

    install sprint-boot

---
+++ src/main/java/com/haru864/thinkvoice/ThinkvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.haru864.thinkvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haru864/thinkvoice/ThinkvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru864.thinkvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
