822effb948df0c7c03a24f2117ee57e5c348afdf
haru <kawachigenji95@gmail.com>
2024-12-10 22:38:55 +0900

    Junitエラー解消

--- src/test/java/com/haru864/thinkvoice/ThinkvoiceApplicationTests.java
+++ src/test/java/com/haru864/thinkvoice/ThinkvoiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.haru864.thinkvoice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ThinkvoiceApplicationTests {
 
